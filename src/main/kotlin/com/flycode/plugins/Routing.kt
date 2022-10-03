6da57bf74372a3f2ee1ca73c9a273990660d2f8c
Iddan Aaronsohn <mail@aniddan.com>
2022-10-03 19:31:12 +0300

    Add check endpoint

--- src/main/kotlin/com/flycode/plugins/Routing.kt
+++ src/main/kotlin/com/flycode/plugins/Routing.kt
@@ -1,27 +1,29 @@
 package com.flycode.plugins
 
 import com.flycode.LanguageToolService
-import io.ktor.server.routing.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import kotlinx.serialization.Serializable
 
-@Serializable
-data class BulkCheckRequest(val texts: List<String>)
+@Serializable data class BulkCheckRequest(val texts: List<String>)
+
+@Serializable data class CheckRequest(val text: String)
 
 fun Application.configureRouting() {
 
     routing {
+        post("/check") {
+            val bulkCheckRequest = call.receive<CheckRequest>()
+
+            call.respond(LanguageToolService.check(bulkCheckRequest.text))
+        }
         post("/bulk-check") {
             val bulkCheckRequest = call.receive<BulkCheckRequest>()
 
-            call.respond(
-                LanguageToolService.check(bulkCheckRequest.texts)
-            )
+            call.respond(LanguageToolService.check(bulkCheckRequest.texts))
         }
-        get("/") {
-            call.respondText("Hello World")
-        }
+        get("/") { call.respondText("Hello World") }
     }
 }
