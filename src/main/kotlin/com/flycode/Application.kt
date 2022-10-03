cebf7113002136f12db0d2198241c19c0aef68f2
Iddan Aaronsohn <mail@aniddan.com>
2022-10-03 20:02:31 +0300

    Improve setup

--- src/main/kotlin/com/flycode/Application.kt
+++ src/main/kotlin/com/flycode/Application.kt
@@ -1,16 +1,17 @@
 package com.flycode
 
+import com.flycode.plugins.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.flycode.plugins.*
 
 fun main() {
-//    val port = System.getenv("PORT")
-    
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureMonitoring()
-        configureSecurity()
-        configureSerialization()
-        configureRouting()
-    }.start(wait = true)
+    val port = System.getenv("PORT").toIntOrNull()
+    embeddedServer(Netty, port = port ?: 8080, host = "0.0.0.0") {
+                LanguageToolService.init()
+                configureMonitoring()
+                configureSecurity()
+                configureSerialization()
+                configureRouting()
+            }
+            .start(wait = true)
 }
