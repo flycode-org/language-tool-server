ee95143b9d19caed065bb0eeeccebcba770dcd2d
Iddan Aaronsohn <mail@aniddan.com>
2022-10-03 19:22:54 +0300

    Lock guava version to newer

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("com.google.guava:guava:31.1-jre")
     implementation("org.languagetool:language-en:5.9")
 
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
--- src/main/kotlin/com/flycode/Application.kt
+++ src/main/kotlin/com/flycode/Application.kt
@@ -5,6 +5,8 @@ import io.ktor.server.netty.*
 import com.flycode.plugins.*
 
 fun main() {
+//    val port = System.getenv("PORT")
+    
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureMonitoring()
         configureSecurity()
