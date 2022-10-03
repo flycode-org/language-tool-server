bf389defe6867efc911da434a76edfa30530eea3
Etai Avrahami <etai@flycode.com>
2022-10-03 16:07:41 +0300

    added bulk-check endpoint

---
+++ src/main/kotlin/com/flycode/LanguageToolService.kt
@@ -0,0 +1,41 @@
+package com.flycode
+
+import kotlinx.serialization.Serializable
+import org.languagetool.JLanguageTool
+import org.languagetool.language.AmericanEnglish
+
+object LanguageToolService {
+    val langTool = JLanguageTool(AmericanEnglish())
+
+    fun init() {
+        langTool.check("Initialize")
+    }
+
+    fun check(text: String): CheckResult {
+        // comment in to use statistical ngram data:
+        // langTool.activateLanguageModelRules(new File("/data/google-ngram-data"));
+        return CheckResult(
+            text, (langTool.check(text) ?: emptyList()).map { ruleMatch ->
+                RuleMatchResult(
+                    ruleMatch.message,
+                    ruleMatch.shortMessage,
+//                    ruleMatch.suggestedReplacements ?: emptyList(),
+                    emptyList()
+                )
+            }
+        )
+    }
+
+    fun check(texts: List<String>): List<CheckResult> {
+        // comment in to use statistical ngram data:
+        // langTool.activateLanguageModelRules(new File("/data/google-ngram-data"));
+        return texts.map { check(it) }
+    }
+
+}
+
+@Serializable
+data class RuleMatchResult(val message: String, val shortMessage: String, val replacements: List<String>)
+
+@Serializable
+data class CheckResult(val text: String, val ruleMatches: List<RuleMatchResult>)
--- src/main/kotlin/com/flycode/plugins/Routing.kt
+++ src/main/kotlin/com/flycode/plugins/Routing.kt
@@ -1,16 +1,27 @@
 package com.flycode.plugins
 
+import com.flycode.LanguageToolService
 import io.ktor.server.routing.*
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.request.*
+import io.ktor.server.response.*
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class BulkCheckRequest(val texts: List<String>)
 
 fun Application.configureRouting() {
 
     routing {
+        post("/bulk-check") {
+            val bulkCheckRequest = call.receive<BulkCheckRequest>()
+
+            call.respond(
+                LanguageToolService.check(bulkCheckRequest.texts)
+            )
+        }
         get("/") {
-            call.respondText("Hello World!")
+            call.respondText("Hello World")
         }
     }
 }
