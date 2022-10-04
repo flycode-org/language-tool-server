bd44667bb84360adb1ca61c202709ddad3f1d435
Iddan Aaronsohn <mail@aniddan.com>
2022-10-04 09:31:41 +0300

    Fix running without PORT

--- src/main/kotlin/com/flycode/Application.kt
+++ src/main/kotlin/com/flycode/Application.kt
@@ -5,7 +5,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    val port = System.getenv("PORT").toIntOrNull()
+    val port = System.getenv("PORT")?.toIntOrNull()
     embeddedServer(Netty, port = port ?: 8080, host = "0.0.0.0") {
                 LanguageToolService.init()
                 configureMonitoring()
